3e0af5d8341b64792792bfa49da56da0675339f2
Walther Carrasco <waemcame@gmail.com>
2021-05-19 22:56:16 +0000

    integer break challenge

---
+++ Integer Break/index.js
@@ -0,0 +1,25 @@
+/*
+Code challenge:
+
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+*/
+
+var integerBreak = function(n) {
+    if(n < 3){return 1}
+    if(n == 3){return 2}
+    let parts = []
+    
+
+    while(n !== 0){
+        if(n > 4){
+            parts.push(3)
+            n = n - 3
+        }else{
+            parts.push(n)
+            n = 0
+        }
+    }
+    return parts.reduce((a,b) => a * b, 1)
+};
